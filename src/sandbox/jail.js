6072bb1296a1d17bcb3f8035a2f5dea7521938e6
GASPARINI Nicolas <nicolas.gasp@free.fr>
2017-05-29 17:50:10 +0200

    scope fix

--- src/sandbox/jail.js
+++ src/sandbox/jail.js
@@ -4,10 +4,9 @@ const vm = require('vm');
  * @param {Context} context 
  */
 module.exports = (context, timeout = 60) => {
-  const scope = context.getScope();
-
   return function(code) {
     return (async () => {
+      const scope = Object.assign({ module: {} }, context.getScope());
       vm.runInNewContext(`'use strict'; module.exports = (async function(){ ${code} })();`, scope, { timeout });
       return await scope.module.exports;
     })();
